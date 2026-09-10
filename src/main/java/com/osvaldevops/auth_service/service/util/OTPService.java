ae5c24a3ceded896de290f6bea583154d2a33c9d
Osvaldo Garcia <osvaldsilent@gmail.com>
2026-09-10 14:58:11 -0600

    fix: refactoring to keep congruency with the arch

--- src/main/java/com/osvaldevops/auth_service/service/util/OTPService.java
+++ src/main/java/com/osvaldevops/auth_service/service/util/OTPService.java
@@ -1,5 +1,8 @@
-package com.osvaldevops.auth_service.service.otp;
+package com.osvaldevops.auth_service.service.util;
 
+import org.springframework.stereotype.Service;
+
+@Service 
 public class OTPService {
     public String generateSecureOtp() {
         return String.valueOf(100000 + new java.security.SecureRandom().nextInt(900000));
